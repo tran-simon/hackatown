a42d85a3a52ef9ccacbf1dc5a4b5e3f0123b2724
Charlie Duquette <charlie_duquette@hotmail.fr>
2019-01-19 16:11:04 -0500

    Ajout de EventCreatorPart2

--- app/src/main/java/com/example/hackatown/MainActivity.java
+++ app/src/main/java/com/example/hackatown/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        Intent intent = new Intent(MainActivity.this, EventCreatorPart2.class);
         startActivity(intent);
 
         Button button = findViewById(R.id.button);
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                Intent intent = new Intent(MainActivity.this, EventCreatorPart2.class);
                 startActivity(intent);
 
 
