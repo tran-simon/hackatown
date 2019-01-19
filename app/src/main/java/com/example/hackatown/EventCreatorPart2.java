ba16d88b6148a4203c5cb27dcb987d5e9750f1fd
Charlie Duquette <charlie_duquette@hotmail.fr>
2019-01-19 17:51:35 -0500

    test

--- app/src/main/java/com/example/hackatown/EventCreatorPart2.java
+++ app/src/main/java/com/example/hackatown/EventCreatorPart2.java
@@ -1,9 +1,15 @@
 package com.example.hackatown;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Date;
 
 public class EventCreatorPart2 extends AppCompatActivity {
 
@@ -14,13 +20,16 @@ public class EventCreatorPart2 extends AppCompatActivity {
 
         //Parametre
         Button sendRequestBtn = findViewById(R.id.sendRequestBtn);
+        Button insertImgBtn = findViewById(R.id.insertImgBtn);
+        final TextInputEditText eventDescription = findViewById(R.id.eventDescription);
 
         sendRequestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //debut
-
-                //fin
+            Request request = new Request( Request.EventType.AbrisBus, eventDescription.getFontFeatureSettings(), new LatLng(0,0), new Date(), 10);
+            Log.d("POLY", Request.EventType.AbrisBus + eventDescription.getFontFeatureSettings() + new LatLng(0,0) + new Date());
+            //fin
             }
         });
     }
