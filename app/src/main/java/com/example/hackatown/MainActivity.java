80ef69678dad65f6cf231714eda5ea23dcb2c144
simon <tran.simon2@gmail.com>
2019-01-19 09:52:49 -0500

    added map

--- app/src/main/java/com/example/hackatown/MainActivity.java
+++ app/src/main/java/com/example/hackatown/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.hackatown;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //test
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
