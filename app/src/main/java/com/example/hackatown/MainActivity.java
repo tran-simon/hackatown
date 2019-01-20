cf03e8842e73087220372b5f50a7c465a1509a7b
Issam Maghni <concatime@users.noreply.github.com>
2019-01-20 01:50:50 -0500

    first try

--- app/src/main/java/com/example/hackatown/MainActivity.java
+++ app/src/main/java/com/example/hackatown/MainActivity.java
@@ -3,16 +3,63 @@ package com.example.hackatown;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+	String mCurrentPhotoPath;
+	private File createImageFile() {
+		// Create an image file name
+		String imageFileName = "JPEG_PICTURE";
+		File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+		File image = null;
+		try {
+			image = File.createTempFile(
+					imageFileName,  /* prefix */
+					".jpg",         /* suffix */
+					storageDir      /* directory */
+			);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Save a file: path for use with ACTION_VIEW intents
+		mCurrentPhotoPath = image.getAbsolutePath();
+		return image;
+	}
+
+	private void dispatchTakePictureIntent() {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		// Ensure that there's a camera activity to handle the intent
+		if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+			// Create the File where the photo should go
+			File photoFile = null;
+			photoFile = createImageFile();
+			// Continue only if the File was successfully created
+			if (photoFile != null) {
+				Uri photoURI = FileProvider.getUriForFile(this,
+						"com.example.hackatown.fileprovider",
+						photoFile);
+				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+				startActivityForResult(takePictureIntent, 1);
+				System.out.println("YESS");
+			}
+		} else {
+			System.out.println("MABAD");
+		}
+	}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +71,10 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
 
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-        startActivity(intent);
+	    dispatchTakePictureIntent();
+
+        //Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        //startActivity(intent);
 
         Button button = findViewById(R.id.button);
 
@@ -41,6 +90,10 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
             }
         });
 
+
+
+
+/*
 	    requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
 
 	    if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
@@ -55,6 +108,6 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
 		    }
 	    } else {
 		    System.out.println("NO PERMISSION");
-	    }
+	    }*/
     }
 }
